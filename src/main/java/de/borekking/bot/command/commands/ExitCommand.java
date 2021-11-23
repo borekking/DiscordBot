21255d25ec58c72b824ed086ab110b31798984fc
borekking <mcnairolfflo@gmail.com>
2021-11-23 21:56:48 +0100

    Added "exit" command to disable the bot

---
+++ src/main/java/de/borekking/bot/command/commands/ExitCommand.java
@@ -0,0 +1,24 @@
+package de.borekking.bot.command.commands;
+
+import de.borekking.bot.Main;
+import de.borekking.bot.command.Command;
+import de.borekking.bot.util.MyEmbedBuilder;
+
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+
+import java.sql.SQLException;
+
+public class ExitCommand extends Command {
+
+    public ExitCommand() {
+        super("exit", "Disables Bot", new OptionData[0], Permission.ADMINISTRATOR);
+    }
+
+    @Override
+    public void perform(SlashCommandEvent event) {
+        Main.getDiscordBot().disableBot();
+        event.replyEmbeds(new MyEmbedBuilder().title("Disabled bot").build()).queue();
+    }
+}
