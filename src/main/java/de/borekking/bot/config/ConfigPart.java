cbc270b26311641427c2bffd702f1a74c361b053
borekking <mcnairolfflo@gmail.com>
2021-11-30 18:40:29 +0100

    Added ConfigPart constructor with ZeroFunction AS value

--- src/main/java/de/borekking/bot/config/ConfigPart.java
+++ src/main/java/de/borekking/bot/config/ConfigPart.java
@@ -1,15 +1,22 @@
 package de.borekking.bot.config;
 
+import de.borekking.bot.util.java.ZeroFunction;
+
 import java.util.function.Function;
 
 public class ConfigPart {
 
-    private String key;
+    private final String key;
     private Object value;
 
     public ConfigPart(String key, Object value) {
         this.key = key;
         this.value = value;
+    }
+
+    public <T> ConfigPart(String key, ZeroFunction<T> value) {
+        this.key = key;
+        this.value = value.apply();
     }
 
     public  <T> T getValue(Function<Object, T> f) {
