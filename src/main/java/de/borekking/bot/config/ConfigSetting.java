483502436b41f4bc476fcc1fdf2e38a266cfaac3
borekking <mcnairolfflo@gmail.com>
2021-12-01 17:08:58 +0100

    Implemented EventInformation in ConfigSetting and added ConfigPart "leaveInformation" using EventInformation (also changed JOIN_MESSAGE to using EventInformation)

--- src/main/java/de/borekking/bot/config/ConfigSetting.java
+++ src/main/java/de/borekking/bot/config/ConfigSetting.java
@@ -1,23 +1,15 @@
 package de.borekking.bot.config;
 
 import de.borekking.bot.util.discord.DefaultEmbeds;
-import de.borekking.bot.util.discord.JSONEmbedUtil;
+import de.borekking.bot.util.discord.event.EventInformation;
 import org.json.simple.JSONObject;
 
 public enum ConfigSetting {
 
     TOKEN(new ConfigPart("token", "")),
     GUILD_ID(new ConfigPart("guildID", "")),
-    JOIN_MESSAGE(new ConfigPart("joinInformation", () -> {
-        JSONObject embed = JSONEmbedUtil.toJSONObject(DefaultEmbeds.JOIN_EMBED.getEmbed());
-
-        JSONObject object = new JSONObject();
-        object.put("enabled", true);
-        object.put("channelID", "");
-        object.put("embed", embed);
-
-        return object;
-    }));
+    JOIN_MESSAGE(new ConfigPart("joinInformation", new EventInformation(true, "", DefaultEmbeds.JOIN_EMBED.getEmbed()).toJSONObject())),
+    LEAVE_MESSAGE(new ConfigPart("leaveInformation", new EventInformation(true, "", DefaultEmbeds.LEAVE_EMBED.getEmbed()).toJSONObject()));
 
     private final ConfigPart part;
 
@@ -30,7 +22,11 @@ public enum ConfigSetting {
     }
 
     public JSONObject getValueAsJSONObject() {
-        return this.part.getValue(o -> (JSONObject) o);
+        return this.part.getValue(o -> o instanceof JSONObject ? (JSONObject) o : null);
+    }
+
+    public EventInformation getAsEventInformation() {
+        return this.part.getValue(o -> EventInformation.getFromJSON(this.getValueAsJSONObject()));
     }
 
     public String getKey() {
