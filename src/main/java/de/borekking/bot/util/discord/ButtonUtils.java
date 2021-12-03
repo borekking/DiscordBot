027b37f017bd415955227719632c45e78d333a39
borekking <mcnairolfflo@gmail.com>
2021-12-03 22:29:43 +0100

    Added Util for Buttons which disables all Button of one Message

---
+++ src/main/java/de/borekking/bot/util/discord/ButtonUtils.java
@@ -0,0 +1,24 @@
+package de.borekking.bot.util.discord;
+
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.interactions.components.ActionRow;
+import net.dv8tion.jda.api.interactions.components.Button;
+
+import java.util.List;
+
+public class ButtonUtils {
+
+    public static void disableButtons(Message msg) {
+        List<ActionRow> actionRows = msg.getActionRows();
+
+        for (ActionRow ar : actionRows) {
+            for (Button b : ar.getButtons()) {
+                String id = b.getId();
+                if (id == null) continue;
+                ar.updateComponent(id, b.asDisabled());
+            }
+        }
+
+        msg.editMessageComponents(actionRows).queue();
+    }
+}
