0ea1b7fc2f8da0b9c430bd835d5d4c880c0cebf6
borekking <mcnairolfflo@gmail.com>
2021-12-04 00:02:09 +0100

    Added optional Option to mention a role (also @everyone) on an announcement.

--- src/main/java/de/borekking/bot/command/commands/AnnouncementCommand.java
+++ src/main/java/de/borekking/bot/command/commands/AnnouncementCommand.java
@@ -6,11 +6,9 @@ import de.borekking.bot.listener.button.ButtonAction;
 import de.borekking.bot.listener.button.ButtonManager;
 import de.borekking.bot.util.discord.MyEmbedBuilder;
 import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.MessageChannel;
-import net.dv8tion.jda.api.entities.MessageEmbed;
-import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.interactions.components.Button;
@@ -19,13 +17,12 @@ import java.awt.Color;
 
 public class AnnouncementCommand extends Command {
 
-    // TODO Make @everyone option
-    
     public AnnouncementCommand() {
         super("announcement", "Make an Announcement to a specified channel", new OptionData[]{
                 new OptionData(OptionType.CHANNEL, "channel", "Channel to post the Announcement in", true),
                 new OptionData(OptionType.STRING, "title", "Title of the announcement-message", true),
-                new OptionData(OptionType.STRING, "text", "Text of the announcement-message", true)
+                new OptionData(OptionType.STRING, "text", "Text of the announcement-message", true),
+                new OptionData(OptionType.ROLE, "pinged-role", "Text of the announcement-message", false)
         }, Permission.ADMINISTRATOR);
     }
 
@@ -38,6 +35,8 @@ public class AnnouncementCommand extends Command {
         MessageChannel targetChannel = event.getOption("channel").getAsMessageChannel();
         String title = event.getOption("title").getAsString();
         String text = event.getOption("text").getAsString();
+        OptionMapping roleOption = event.getOption("pinged-role");
+        Role pingedRole = roleOption != null ? roleOption.getAsRole() : null;
 
         // Make sure message is only sent to a TextChannel
         if (!(targetChannel instanceof TextChannel)) {
@@ -62,7 +61,11 @@ public class AnnouncementCommand extends Command {
 
         // Create ButtonAction
         ButtonAction buttonAction = new ButtonAction(id);
-        buttonAction.addAction(sendButton, (member1) -> targetChannel.sendMessageEmbeds(embed).queue());
+        buttonAction.addAction(sendButton, (member1) -> {
+            targetChannel.sendMessageEmbeds(embed).queue();
+            if (pingedRole != null)
+                targetChannel.sendMessage(pingedRole.getAsMention()).queue();
+        });
         buttonAction.addAction(cancelButton, (member1) ->
                 senderChannel.sendMessageEmbeds(new MyEmbedBuilder().color(Color.RED).title("Canceled").description("Canceled announcement.").build()).queue());
         Main.getButtonManager().addButtonEvent(member, buttonAction);
