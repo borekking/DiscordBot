f24c7326a6e5d5180cc052badfed8cb4b4873c78
borekking <mcnairolfflo@gmail.com>
2021-12-05 00:13:03 +0100

    Added HelpCommand which shows the name and description of every command the member has permissions for as field.

---
+++ src/main/java/de/borekking/bot/command/commands/HelpCommand.java
@@ -0,0 +1,28 @@
+package de.borekking.bot.command.commands;
+
+import de.borekking.bot.Main;
+import de.borekking.bot.command.Command;
+import de.borekking.bot.util.discord.embed.EmbedType;
+import de.borekking.bot.util.discord.embed.MyEmbedBuilder;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+
+import java.time.OffsetDateTime;
+
+public class HelpCommand extends Command {
+
+    public HelpCommand() {
+        super("help", "Information about the bot's commands", new OptionData[0]);
+    }
+
+    @Override
+    public void perform(SlashCommandEvent event) {
+        MyEmbedBuilder builder = new MyEmbedBuilder(EmbedType.NEUTRAL).title("Commands Help").timestamp(OffsetDateTime.now());
+
+        for (Command command : Main.getDiscordBot().getCommandList())
+            if (event.getMember().hasPermission(command.getPermissions()))
+                builder.field(command.getName(), command.getDescription(), false);
+
+        event.replyEmbeds(builder.build()).queue();
+    }
+}
