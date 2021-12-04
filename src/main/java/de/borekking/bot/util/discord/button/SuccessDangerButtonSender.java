00ab21c5195e2fdb715a96fdc9e6470031b71a4f
borekking <mcnairolfflo@gmail.com>
2021-12-04 23:24:03 +0100

    Added SuccessDangerButtonSender class to send a message with two buttons that apply a consumer using ButtonManager, ButtonToActionStorage. Class provides methode to actually activate Buttons (use) and to getButtons as arrays.

---
+++ src/main/java/de/borekking/bot/util/discord/button/SuccessDangerButtonSender.java
@@ -0,0 +1,46 @@
+package de.borekking.bot.util.discord.button;
+
+import de.borekking.bot.Main;
+
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.interactions.components.Button;
+
+import java.util.function.Consumer;
+
+public class SuccessDangerButtonSender {
+
+    // TODO Make n Buttons possible (ButtonActionSender)
+
+    private final long id;
+    private final Button successButton, cancelButton;
+    private final Consumer<Member> onSuccess, onCancel;
+
+    public SuccessDangerButtonSender(String successTitle, Consumer<Member> onSuccess, String dangerTitle, Consumer<Member> onCancel) {
+        ButtonManager manager = Main.getButtonManager();
+
+        // Get new unique id
+        this.id = manager.getNewUniqueID();
+
+        // Created buttons with id
+        String[] buttonID = manager.getIDs(this.id, 2);
+        this.successButton = Button.success(buttonID[0], successTitle);
+        this.cancelButton = Button.danger(buttonID[1], dangerTitle);
+
+        // Set consumers/actions
+        this.onSuccess = onSuccess;
+        this.onCancel = onCancel;
+    }
+
+    public void use(Member member) {
+        // Create ButtonAction
+        ButtonToActionStorage buttonAction = new ButtonToActionStorage(this.id);
+        buttonAction.addAction(this.successButton, this.onSuccess);
+        buttonAction.addAction(this.cancelButton, this.onCancel);
+
+        Main.getButtonManager().addButtonEvent(member, buttonAction);
+    }
+
+    public Button[] getButtons() {
+        return new Button[]{this.successButton, this.cancelButton};
+    }
+}
