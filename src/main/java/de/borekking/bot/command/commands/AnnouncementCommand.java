5d00bd03d2d277f26562d781e04a8bcca0ca7872
borekking <mcnairolfflo@gmail.com>
2021-12-04 23:28:24 +0100

    Used new SuccessDangerButtonSender class in AnnouncementCommand.

--- src/main/java/de/borekking/bot/command/commands/AnnouncementCommand.java
+++ src/main/java/de/borekking/bot/command/commands/AnnouncementCommand.java
@@ -2,18 +2,17 @@ package de.borekking.bot.command.commands;
 
 import de.borekking.bot.Main;
 import de.borekking.bot.command.Command;
-import de.borekking.bot.listener.button.ButtonAction;
-import de.borekking.bot.listener.button.ButtonManager;
-import de.borekking.bot.util.discord.MyEmbedBuilder;
+import de.borekking.bot.util.discord.button.SuccessDangerButtonSender;
+import de.borekking.bot.util.discord.embed.MyEmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
-import net.dv8tion.jda.api.interactions.components.Button;
 
 import java.awt.Color;
+import java.util.function.Consumer;
 
 public class AnnouncementCommand extends Command {
 
@@ -49,25 +48,18 @@ public class AnnouncementCommand extends Command {
         builder.replace(Main.getPlaceholderTranslator());
         MessageEmbed embed = builder.build();
 
-        // Create buttons
-        ButtonManager manager = Main.getButtonManager();
-        long id = manager.getNewUniqueID();
-        String[] buttonID = manager.getIDs(id, 2);
-        Button sendButton = Button.success(buttonID[0], "SEND");
-        Button cancelButton = Button.success(buttonID[1], "CANCEL");
-
-        // Reply to Event
-        event.reply("This is your message:").addEmbeds(embed).addActionRow(sendButton, cancelButton).queue();
-
-        // Create ButtonAction
-        ButtonAction buttonAction = new ButtonAction(id);
-        buttonAction.addAction(sendButton, (member1) -> {
+        Consumer<Member> onSuccess = (member1) -> {
             targetChannel.sendMessageEmbeds(embed).queue();
             if (pingedRole != null)
                 targetChannel.sendMessage(pingedRole.getAsMention()).queue();
-        });
-        buttonAction.addAction(cancelButton, (member1) ->
-                senderChannel.sendMessageEmbeds(new MyEmbedBuilder().color(Color.RED).title("Canceled").description("Canceled announcement.").build()).queue());
-        Main.getButtonManager().addButtonEvent(member, buttonAction);
+        }, onCancel = (member1) ->
+                senderChannel.sendMessageEmbeds(new MyEmbedBuilder().color(Color.RED).title("Canceled").description("Canceled announcement.").build()).queue();
+
+        SuccessDangerButtonSender sender = new SuccessDangerButtonSender("SEND", onSuccess, "CANCEL", onCancel);
+
+        // Reply to Event
+        event.reply("This is your message:").addEmbeds(embed).addActionRow(sender.getButtons()).queue();
+
+        sender.use(member);
     }
 }
