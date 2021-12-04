5e881e0206969b97237aa8af2c8e90c4fd2dea20
borekking <mcnairolfflo@gmail.com>
2021-12-04 23:33:17 +0100

    Added methods in PlaceholderTranslator which take new Placeholders and return a new instance of the PlaceholderTranslator which these new placeholders.

--- src/main/java/de/borekking/bot/util/placeholder/PlaceholderTranslator.java
+++ src/main/java/de/borekking/bot/util/placeholder/PlaceholderTranslator.java
@@ -1,6 +1,12 @@
 package de.borekking.bot.util.placeholder;
 
+import de.borekking.bot.util.placeholder.placeholderTypes.GeneralPlaceholder;
+import de.borekking.bot.util.placeholder.placeholderTypes.MemberPlaceholder;
 import net.dv8tion.jda.api.entities.Member;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class PlaceholderTranslator {
 
@@ -21,4 +27,40 @@ public class PlaceholderTranslator {
         return this.generalPlaceholderManager.translate(msg);
     }
 
+    // Methods to get this PlaceholderTranslator with extra placeholder(s)
+    public PlaceholderTranslator getWith(List<GeneralPlaceholder> generalPlaceholders, List<MemberPlaceholder> memberPlaceholders) {
+        PlaceholderManager<Void> manager1 = this.generalPlaceholderManager.copy();
+        for (GeneralPlaceholder ph : generalPlaceholders) {
+            if (ph == null) continue;
+            manager1.addPlaceholder(ph);
+        }
+
+        PlaceholderManager<Member> manager2 = this.memberPlaceholderManager.copy();
+        for (MemberPlaceholder ph : memberPlaceholders) {
+            if (ph == null) continue;
+            manager2.addPlaceholder(ph);
+        }
+
+        return new PlaceholderTranslator(manager1, manager2);
+    }
+
+    public PlaceholderTranslator getWithGeneralPHs(List<GeneralPlaceholder> generalPlaceholders) {
+        return this.getWith(generalPlaceholders, new ArrayList<>());
+    }
+
+    public PlaceholderTranslator getWithMemberPHs(List<MemberPlaceholder> memberPlaceholders) {
+        return this.getWith(new ArrayList<>(), memberPlaceholders);
+    }
+
+    public PlaceholderTranslator getWithPHs(GeneralPlaceholder ph0, MemberPlaceholder ph1) {
+        return this.getWith(Collections.singletonList(ph0), Collections.singletonList(ph1));
+    }
+
+    public PlaceholderTranslator getWithGeneralPH(GeneralPlaceholder ph) {
+        return this.getWithPHs(ph, null);
+    }
+
+    public PlaceholderTranslator getWithMemberPH(MemberPlaceholder ph) {
+        return this.getWithPHs(null, ph);
+    }
 }
