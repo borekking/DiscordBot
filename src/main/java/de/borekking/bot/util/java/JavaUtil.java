ef25024fc74fb26135920d0011dccaffa8ca3a9f
borekking <mcnairolfflo@gmail.com>
2021-12-08 22:06:30 +0100

    Added copy Methode to JavaUtils which create a new instance of a List.

--- src/main/java/de/borekking/bot/util/java/JavaUtil.java
+++ src/main/java/de/borekking/bot/util/java/JavaUtil.java
@@ -1,5 +1,6 @@
 package de.borekking.bot.util.java;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -8,5 +9,9 @@ public class JavaUtil {
 
     public static <T> List<T> getAsList(T[] arr) {
         return Arrays.stream(arr).collect(Collectors.toList());
+    }
+
+    public static <T> List<T> copy(List<T> arr) {
+        return new ArrayList<>(arr);
     }
 }
