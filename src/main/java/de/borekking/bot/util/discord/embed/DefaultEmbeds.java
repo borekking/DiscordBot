3648ba91890072c898ecf0f69ebc05bbd1e6f530
borekking <mcnairolfflo@gmail.com>
2021-12-08 16:17:13 +0100

    Changed EmbedType of DefaultEmbeds "BAN"- and "KICK_INFORMATION" to ERROR (Red Color)

--- src/main/java/de/borekking/bot/util/discord/embed/DefaultEmbeds.java
+++ src/main/java/de/borekking/bot/util/discord/embed/DefaultEmbeds.java
@@ -1,21 +1,18 @@
 package de.borekking.bot.util.discord.embed;
 
-import java.awt.Color;
 import java.time.OffsetDateTime;
 
 public enum DefaultEmbeds {
-
-    // TODO Make BAN, KICK, MUTE of Type ERROR
 
     JOIN_EMBED(new MyEmbedBuilder(EmbedType.NEUTRAL).description("Welcome to **%servername%**, %user%!%nextLine%You are the %memberCount%. member!").title("Welcome!").timestamp(OffsetDateTime.now())),
     LEAVE_EMBED(new MyEmbedBuilder(EmbedType.NEUTRAL).description("Bye, bye, %user%!%nextLine%%nextLine%Left members: %memberCount%").title("Bye!").timestamp(OffsetDateTime.now())),
     EXIT_EMBED(new MyEmbedBuilder(EmbedType.NEUTRAL).title("Disabled bot")),
     RELOAD_EMBED(new MyEmbedBuilder(EmbedType.NEUTRAL).title("Reload").description("Reloaded bot").timestamp(OffsetDateTime.now())),
-    BAN_INFORMATION(new MyEmbedBuilder().color(Color.RED).title("Ban").description("%user% was banned.").field("Reason", "%reason%.", true)
+    BAN_INFORMATION(new MyEmbedBuilder(EmbedType.ERROR).title("Ban").description("%user% was banned.").field("Reason", "%reason%.", true)
             .timestamp(OffsetDateTime.now())),
     UNBAN_INFORMATION(new MyEmbedBuilder(EmbedType.SUCCESS).title("Unban").description("%user% was banned.").field("Reason", "%reason%.", true)
             .timestamp(OffsetDateTime.now())),
-    KICK_INFORMATION(new MyEmbedBuilder(EmbedType.SUCCESS).title("Kick").description("%user% was kicked.").field("Reason", "%reason%.", true)
+    KICK_INFORMATION(new MyEmbedBuilder(EmbedType.ERROR).title("Kick").description("%user% was kicked.").field("Reason", "%reason%.", true)
             .timestamp(OffsetDateTime.now())),
     MUTE_INFORMATION(new MyEmbedBuilder(EmbedType.ERROR).title("Mute").description("%user% was muted.").field("Reason", "%reason%.", true)
             .timestamp(OffsetDateTime.now()));
