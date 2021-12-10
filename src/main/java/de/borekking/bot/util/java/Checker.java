f4d43e25ce6fb581f3ea9caa0e1b2a5818e43d71
borekking <mcnairolfflo@gmail.com>
2021-12-10 17:18:50 +0100

    Added List Checks in "Checker"

--- src/main/java/de/borekking/bot/util/java/Checker.java
+++ src/main/java/de/borekking/bot/util/java/Checker.java
@@ -1,5 +1,9 @@
 package de.borekking.bot.util.java;
 
+import java.util.Collection;
+import java.util.Objects;
+import java.util.function.Predicate;
+
 public class Checker {
 
     public static boolean isEmpty(String str) {
@@ -8,5 +12,20 @@ public class Checker {
 
     public static boolean isNull(Object o) {
         return o == null;
+    }
+
+    // If a test IS true for any element returns false
+    // If one or more tests and empty List: returns false
+    @SafeVarargs
+    public static <T> boolean testList(Collection<T> list, Predicate<T>... tests) {
+        for (Predicate<T> test : tests)
+            if (list.stream().anyMatch(test))
+                return false;
+        return true;
+    }
+
+    public static <T> boolean listNonNull(Collection<T> list) {
+        if (isNull(list)) return false;
+        return testList(list, Objects::isNull);
     }
 }
