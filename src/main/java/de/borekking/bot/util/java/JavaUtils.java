8adfcd4b32dd59c44f84669d62789b205ebb2770
borekking <mcnairolfflo@gmail.com>
2021-12-10 17:17:04 +0100

    JavaUtils: Added addAlIf methode (Adds elements of one list in another with a Predicate), added check for empty list in getAsList Methode

--- src/main/java/de/borekking/bot/util/java/JavaUtils.java
+++ src/main/java/de/borekking/bot/util/java/JavaUtils.java
@@ -3,15 +3,23 @@ package de.borekking.bot.util.java;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class JavaUtils {
 
     public static <T> List<T> getAsList(T[] arr) {
+        if (arr == null || arr.length == 0) return new ArrayList<>();
         return Arrays.stream(arr).collect(Collectors.toList());
     }
 
-    public static <T> List<T> copy(List<T> arr) {
-        return new ArrayList<>(arr);
+    public static <T> List<T> copy(List<T> list) {
+        return new ArrayList<>(list);
+    }
+
+    public static <T> void addAllIf(List<T> target, List<T> scr, Predicate<T> test) {
+        for (T t : scr)
+            if (test.test(t))
+                target.add(t);
     }
 }
